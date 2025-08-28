c97b1b5896ebf3e355848f829ac441bca6b30bbb
emanuelamaya200-source <emanuelamaya200@gmail.com>
2025-08-28 19:38:27 -0300

    Temperatura Base 1.0
    temperatura base

---
+++ TrabajoPractico3Temperatura/src/trabajopractico3temperatura/TrabajoPractico3Temperatura.java
@@ -0,0 +1,19 @@
+
+package trabajopractico3temperatura;
+
+/*
+Integrantes:  
+Amaya Leandro 
+D'Agata Fabian
+Teo Nicolas
+Daiana Sosa
+Lucas Serrano
+ */
+
+public class TrabajoPractico3Temperatura {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
